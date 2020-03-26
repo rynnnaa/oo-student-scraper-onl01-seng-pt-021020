c81c0144c05bbb06be99dafacc90a750dd762a48
Ryna Truong <rynatruong@outlook.com>
2020-03-26 02:28:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do 
+      Student.new 
   end
 
   def add_student_attributes(attributes_hash)
